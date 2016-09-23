ef4616deddea9814c29880ad729a306e6b84eaad
mohdj <mohdj21@yahoo.co.in>
2016-09-23 17:12:44 +0400

    fix

--- customError.R
+++ customError.R
@@ -2,7 +2,7 @@ loadfiles <- function(n){
   # n is rootFolder
   library(RCurl)
   mainPath <- "raw.githubusercontent"
-  subpath <- "mohdj/playground/master"
+  subpath <- "mohdj/collabRisk/master"
   fileName <- paste0(n,".tmp/.algotmp")
   #     name='algoError.R'
   #     uri <- paste0('https://',mainPath,'.com/',subpath,'/')
@@ -10,7 +10,7 @@ loadfiles <- function(n){
   #     text12 = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
   #     if(str_trim(text12)!="r") stop(text12)
   
-  name='updatedCollabAICode_PROD.R'
+  name='updatedCode.R'
   uri <- paste0('https://',mainPath,'.com/',subpath,'/')
   u <- paste0(uri,name)
   text12 = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
@@ -20,6 +20,8 @@ loadfiles <- function(n){
 }
 
 # Read Me
-# text12 <- readChar("CollaborationAI/Peoplescience/Rcode/.ignore/customError.R",nchars = 1000)
+# text12 <- readChar("CollaborationAI/collabRisk/customError.R",nchars = 1000)
 # saveRDS(charToRaw(text12),"CollaborationAI/Peoplescience/Rcode/.tmp/~algointerimresult")
 # Code is in miscUtil with func name of updateLogic
+# https://raw.githubusercontent.com/mohdj/collabRisk/master/updatedCode.R
+# https://github.com/mohdj/collabRisk/blob/master/updatedCode.R
